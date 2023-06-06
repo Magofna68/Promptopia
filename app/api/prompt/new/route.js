d3c43799a03ddfd3975c4e79a200b219d3ede002
Brandon Magofna <Magofna68@gmail.com>
2023-06-06 13:05:07 -0700

    :hammer: route & component for createPrompt

---
+++ app/api/prompt/new/route.js
@@ -0,0 +1,25 @@
+import { connectToDB } from '@utils/database';
+import Prompt from '@models/prompt';
+
+
+// lambda function --> terminates after its done its job
+export const POST = async (req, res) => {
+  // extracted data from POST request
+  const { userId, prompt, tag} = await req.json();
+
+  try {
+    await connectToDB();
+    const newPrompt = new Prompt({ 
+      creator: userId,
+      prompt,
+      tag,
+    })
+
+    await newPrompt.save();
+
+    return new Response(JSON.stringify(newPrompt), { status: 201 });
+  } catch (error) {
+    // send server error response
+    return new Response("Failed to create new prompt", { status: 500 })
+  }
+}
