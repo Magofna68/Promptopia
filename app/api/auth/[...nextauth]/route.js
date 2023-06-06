5fffc6d829416431876a19417bff82ae7b436d1f
Brandon Magofna <Magofna68@gmail.com>
2023-06-06 11:46:53 -0700

    update .env file for proper configuration to MongoDB

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,45 +4,49 @@ import { connectToDB } from "@utils/database";
 import User from "@models/user";
 
 const handler = NextAuth({
+  // next authentication -> google
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     })
   ],
-  async session({ session }) {
+  callbacks: {
+    async session({ session }) {
 
-    const sessionUser = await User.findOne({
-      email: session.user.email,
-    });
-
-    session.user.id = sessionUser._id.toString();
-
-    return session;
-  },
-  async signIn ({ profile }) {
-    try {
-      // serverless -> lambda function -> dynamodb
-      await connectToDB();
-      // check to see if user already exists
-      const userExists = await User.findOne({
-        email: profile.email
+      const sessionUser = await User.findOne({
+        email: session.user.email,
       });
 
-      // if no user found -> create user
-      if (!userExists) {
-        await User.create({
-          email: profile.email,
-          username: profile.name.replace(" ", "").toLowerCase(),
-          image: profile.picture,
-        })
+      session.user.id = sessionUser._id.toString();
+
+      return session;
+    },
+    
+    async signIn ({ profile }) {
+      try {
+        // serverless -> lambda function -> dynamodb
+        await connectToDB();
+        // check to see if user already exists
+        const userExists = await User.findOne({
+          email: profile.email
+        });
+
+        // if no user found -> create user
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          })
+        }
+        // check if user already exists
+        // if not, create new user and save to DB
+        return true;
+      } catch (error) {
+        console.log("Error signing in: ", error);
+        return false;
       }
-      // check if user already exists
-      // if not, create new user and save to DB
-      return true;
-    } catch (error) {
-      console.log("Error signing in: ", error);
-      return false;
     }
   }
 })
