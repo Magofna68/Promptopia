e12fca67af93b8b6d9b60db8055bcf7418ed03d8
Brandon Magofna <Magofna68@gmail.com>
2023-06-06 15:23:01 -0700

    update: Create, Read, Update functional; :hammer: out Delete next

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -7,8 +7,12 @@ import { usePathname, useRouter } from 'next/navigation';
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const [copied, setCopied ] = useState("");
+  const { data: session } = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
 
   const handleCopy = () => {
+
     setCopied(post.prompt);
     navigator.clipboard.writeText(post.prompt);
     setTimeout(() => setCopied(""), 3000);
@@ -56,8 +60,25 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       >
         {post.tag}
       </p>
+      {/* {console.log('Session.user.Id: ', session.user.id, "post.creator.id: ", post.creator._id)} */}
+      {session?.user.id === post.creator._id && pathName === "/profile" && (
+        <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
+          <p
+            className='font-inter text-sm green_gradient cursor-pointer'
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className='font-inter text-sm orange_gradient cursor-pointer'
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default PromptCard
